fcefd725b53af32883c0d7e0e802a7e41f69c849
Elanthiraiyan <elanthiraiyan.thiagarajan@gmail.com>
2024-07-21 11:33:26 +0530

    Update FindMissingNumber.java

--- datastructurelearning/src/dspracticeproblems/FindMissingNumber.java
+++ datastructurelearning/src/dspracticeproblems/FindMissingNumber.java
@@ -4,7 +4,7 @@ public class FindMissingNumber {
 
     public static void main(String[] args) {
         int[] arr = new int[]{0,1,2,3,5};
-        System.out.println(missingNumber(arr));
+        System.out.println(missingNumberUsingXor(arr,arr.length+1));
     }
 
     public static int missingNumber(int[] numbs) {
@@ -17,5 +17,36 @@ public class FindMissingNumber {
         }
 
         return expectedSum - actualSum;
+    }
+
+    public static int missingNumberUsingForLoops(int[] numbs){
+        int[] tempArray = new int[numbs.length+1];
+
+        for(int i=0;i<numbs.length-1;i++){
+            tempArray[numbs[i]]++;
+        }
+
+        for(int j=0;j<=numbs.length;j++){
+            if(tempArray[j]==0){
+                return j;
+            }
+        }
+
+        return 0;
+    }
+
+    public static int missingNumberUsingXor(int[] numbs,int n){
+
+        int xor1 =0,xor2=0;
+
+        for (int i=0;i<numbs.length;i++){
+            xor2^= numbs[i];
+        }
+
+        for(int i=0;i<numbs.length+1;i++){
+            xor1 ^= i;
+        }
+
+        return xor1^xor2;
     }
 }
